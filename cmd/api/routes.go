9df4128452010fb8851953682079b0e22a5ea654
maz <maziar.mir.2002@gmail.com>
2026-09-24 13:42:31 +0330

    implemented metric capturing

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -22,7 +23,10 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
-	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
+	router.HandlerFunc(http.MethodGet, "/debug/vars", expvar.Handler().ServeHTTP)
+
+	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
