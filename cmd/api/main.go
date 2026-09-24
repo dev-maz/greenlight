4827a5581edc472cf983510694a60fa34a506ba9
maz <maziar.mir.2002@gmail.com>
2026-09-24 12:03:17 +0330

    implemented handling of CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"greenlight/internal/jsonlog"
 	"greenlight/internal/mailer"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -67,6 +71,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "213c67bd9610d2", "smtp username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "efb3d2ac50e42f", "smtp password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "greenlight <no-reply@greenlight.alexedwards.net>", "smtp sender")
+
+	flag.Func("cors-trusted-origins", "trusted cors origins (space seperated)", func(s string) error {
+		cfg.cors.trustedOrigins = strings.Fields(s)
+		return nil
+	})
 
 	flag.Parse()
 
